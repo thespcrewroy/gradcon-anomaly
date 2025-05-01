0ac93f8197d9e582cf7fefea2179d17fd935556e
Swapnil Roy <68826225+thespcrewroy@users.noreply.github.com>
2025-05-01 01:37:32 -0400

    Added banner

--- prep_datasets.py
+++ prep_datasets.py
@@ -1,47 +1,79 @@
-import argparse
-import os
-import random
-import errno
+'Importing libraries'
+import argparse # For argument parsing
+import os # For file and directory operations
+import random # For random sampling
+import errno # For error handling
 
-from torchvision import datasets
-import numpy as np
-import pickle
-from PIL import Image
+from torchvision import datasets # For loading datasets
+import numpy as np  # For numerical operations
+import pickle # For saving and loading data
+from PIL import Image # For image processing
 
 
-parser = argparse.ArgumentParser(description='Download datasets and create splits')
-parser.add_argument('--dataset', default='', type=str, help='Dataset to be downloaded (e.g. cifar-10, mnist, fmnist)')
-parser.add_argument('--save_dir', default='./datasets', type=str, help='Path to save the data')
-parser.add_argument('--outlier_ratio', default=50, type=int, help='Outlier ratio in the test set')
+parser = argparse.ArgumentParser(description='Download datasets and create splits') # Create an argument parser
+parser.add_argument('--dataset', default='', type=str, help='Dataset to be downloaded or preprocessed (e.g. cifar-10, mnist, fmnist, maldeb)') # Dataset name
+parser.add_argument('--save_dir', default='./datasets', type=str, help='Path to save the data') # Directory to save the dataset
+parser.add_argument('--outlier_ratio', default=50, type=int, help='Outlier ratio in the test set') # Ratio of outliers in the test set
 
 
 def main():
+    args = parser.parse_args() # Parse the command line arguments
 
-    args = parser.parse_args()
+    if args.dataset not in ['cifar-10', 'mnist', 'fmnist', 'maldeb']: # Check if the dataset is valid
+        raise ValueError('Dataset should be one of the followings: cifar-10, mnist, fmnist, madleb')
 
-    if args.dataset not in ['cifar-10', 'mnist', 'fmnist']:
-        raise ValueError('Dataset should be one of the followings: cifar-10, mnist, fmnist')
+    if args.dataset != 'maldeb': # If the dataset is not 'maldeb'
+        dataset_dir = os.path.join(args.save_dir, args.dataset) # Directory to save the dataset
+        split_dir = os.path.join(dataset_dir, 'splits')  # Folder where train, val, test splits are saved
+        try: # Create the directory for splits
+            os.makedirs(split_dir) # Create the directory if it doesn't exist
+        except OSError as exception: # Handle the error if the directory already exists
+            if exception.errno != errno.EEXIST: # If the error is not "directory already exists"
+                raise # Raise the error
 
-    dataset_dir = os.path.join(args.save_dir, args.dataset)
-    split_dir = os.path.join(dataset_dir, 'splits')  # Folder where train, val, test splits are saved
-    try:
-        os.makedirs(split_dir)
-    except OSError as exception:
-        if exception.errno != errno.EEXIST:
-            raise
+    if args.dataset == 'cifar-10': # If the dataset is 'cifar-10'
+        trainset = datasets.CIFAR10(dataset_dir, download=True, train=True) # Load the CIFAR-10 training set
+        testset = datasets.CIFAR10(dataset_dir, download=True, train=False) # Load the CIFAR-10 test set
 
-    if args.dataset == 'cifar-10':
-        trainset = datasets.CIFAR10(dataset_dir, download=True, train=True)
-        testset = datasets.CIFAR10(dataset_dir, download=True, train=False)
+    elif args.dataset == 'mnist': # If the dataset is 'mnist'
+        trainset = datasets.MNIST(dataset_dir, download=True, train=True) # Load the MNIST training set
+        testset = datasets.MNIST(dataset_dir, download=True, train=False) # Load the MNIST test set
 
-    elif args.dataset == 'mnist':
-        trainset = datasets.MNIST(dataset_dir, download=True, train=True)
-        testset = datasets.MNIST(dataset_dir, download=True, train=False)
+    elif args.dataset == 'fmnist': # If the dataset is 'fmnist'
+        trainset = datasets.FashionMNIST(dataset_dir, download=True, train=True) # Load the Fashion-MNIST training set
+        testset = datasets.FashionMNIST(dataset_dir, download=True, train=False) # Load the Fashion-MNIST test set
 
-    elif args.dataset == 'fmnist':
-        trainset = datasets.FashionMNIST(dataset_dir, download=True, train=True)
-        testset = datasets.FashionMNIST(dataset_dir, download=True, train=False)
+    elif args.dataset == 'maldeb': # If the dataset is 'maldeb'
+        raw_root = os.path.join(args.save_dir, 'Maldeb')  # where the original Benign/Malicious folders are
+        out_root = os.path.join(args.save_dir, 'maldeb')  # destination split folder
 
+        train_ratio = 0.7 # Ratio of training data
+        val_ratio = 0.15 # Ratio of validation data
+        test_ratio = 0.15 # Ratio of test data
+
+        for split in ['train', 'val', 'test']:
+            os.makedirs(os.path.join(out_root, split, 'Benign'), exist_ok=True)
+            os.makedirs(os.path.join(out_root, split, 'Malicious'), exist_ok=True)
+
+        for cls in ['Benign', 'Malicious']:
+            full_cls_dir = os.path.join(raw_root, cls)
+            files = sorted(os.listdir(full_cls_dir))
+            random.shuffle(files)
+            n = len(files)
+
+            train_files = files[:int(n * train_ratio)]
+            val_files = files[int(n * train_ratio):int(n * (train_ratio + val_ratio))]
+            test_files = files[int(n * (train_ratio + val_ratio)):]
+
+            for f in train_files:
+                shutil.copy(os.path.join(full_cls_dir, f), os.path.join(out_root, 'train', cls, f))
+            for f in val_files:
+                shutil.copy(os.path.join(full_cls_dir, f), os.path.join(out_root, 'val', cls, f))
+            for f in test_files:
+                shutil.copy(os.path.join(full_cls_dir, f), os.path.join(out_root, 'test', cls, f))
+
+        print("Maldeb dataset has been split into train/val/test under:", out_root)
+        return
     np_train = []
     np_test = []
     for img, label in trainset:
